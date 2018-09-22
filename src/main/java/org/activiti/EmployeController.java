4db5093c5a68d377f2ae921cae8c0045d15698c7
monoem <moyahoo.fr>
2018-09-23 00:19:29 +0200

    save dev

--- src/main/java/org/activiti/EmployeController.java
+++ src/main/java/org/activiti/EmployeController.java
@@ -1,18 +1,33 @@
 package org.activiti;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Controller
 public class EmployeController {
 
+	@Autowired
+	 private ApplicantRepository applicantRepository;
+	
 	@RequestMapping(value  = "test", method = RequestMethod.GET)
 	public String addNewPriority(Map<String, Object> model){
 		model.put("message", "hello world");
 		return "test";
 	}
+	
+
+	
+	
+	
+	
+	
+	
 }
