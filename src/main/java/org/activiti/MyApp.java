aef2a077ff913435265515c7786b92dac6a9517e
abdessalem <belhadj.abdessalem@gmail.com>
2018-09-22 21:29:39 +0200

    integrate mysql database

--- src/main/java/org/activiti/MyApp.java
+++ src/main/java/org/activiti/MyApp.java
@@ -33,23 +33,23 @@ public class MyApp {
 //
 //    }
 
-    @Bean
-    InitializingBean usersAndGroupsInitializer(final IdentityService identityService) {
-
-        return new InitializingBean() {
-            public void afterPropertiesSet() throws Exception {
-
-                Group group = identityService.newGroup("user");
-                group.setName("users");
-                group.setType("security-role");
-                identityService.saveGroup(group);
-
-                User admin = identityService.newUser("admin");
-                admin.setPassword("admin");
-                identityService.saveUser(admin);
-
-            }
-        };
-    }
+//    @Bean
+//    InitializingBean usersAndGroupsInitializer(final IdentityService identityService) {
+//
+//        return new InitializingBean() {
+//            public void afterPropertiesSet() throws Exception {
+//
+//                Group group = identityService.newGroup("user");
+//                group.setName("users");
+//                group.setType("security-role");
+//                identityService.saveGroup(group);
+//
+//                User admin = identityService.newUser("admin");
+//                admin.setPassword("admin");
+//                identityService.saveUser(admin);
+//
+//            }
+//        };
+//    }
 
 }
